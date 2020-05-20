b0a0069b4e9507ebfde7aaa180dbd651357e75dc
Twin Karmakharm <hi@twin.uk.com>
2020-05-20 18:36:32 +0100

    rule to build individual .cpp and all tests in the tests folder

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,15 +1,19 @@
-#define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( 1 == 1 );
+    REQUIRE( 1 == 1 );
+    REQUIRE( 4 == 2 );
+    REQUIRE( 6 == 6 );
+    REQUIRE( 3628800 == 3628800 );
+    
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(0) == 1 );
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "Factorials are computed2", "[factorial]" ) {
+ REQUIRE( 1 == 1 );
+    REQUIRE( 1 == 1 );
+    REQUIRE( 4 == 2 );
+    REQUIRE( 6 == 6 );
+    REQUIRE( 3628800 == 3628800 );
     
 }
---
+++ tests/tests2.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch.hpp>
+
+
+TEST_CASE( "Factorials are computed3", "[factorial]" ) {
+    REQUIRE( 1 == 1 );
+    REQUIRE( 1 == 1 );
+    REQUIRE( 4 == 2 );
+    REQUIRE( 6 == 6 );
+    REQUIRE( 3628800 == 3628800 );
+    
+}
